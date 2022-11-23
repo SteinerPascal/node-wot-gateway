c4357c98a8e8bf9199eda786ca5c73a60f77dd20
SteinerPascal <st_pascal@outlook.com>
2022-11-23 15:08:39 +0000

    add mocking actionhandler

--- src/entry.ts
+++ src/entry.ts
@@ -18,5 +18,18 @@ servient.start().then((WoT) => {
     WoT.produce(irrigatorTd)
     .then((thing)=>{
         console.log(`Produced thing: ${JSON.stringify(thing.getThingDescription())}`)
+
+        thing.setPropertyReadHandler("healthy",async (value)=>{
+            return true
+        })
+
+        thing.setPropertyReadHandler("waterlevel",async (value)=>{
+            return 0.8
+        })
+
+        thing.setActionHandler("irrigatePlant",async (value)=>{
+            return 0
+        })
+
     })
 });
